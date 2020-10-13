ffc9c2dfb588b69378c5d4f6d844c07cff163dcf
CaptainVikkus <jaketreleaven@gmail.com>
2020-10-13 11:36:45 -0400

    Lab4 Submit

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float horizontalSpeed;
+
+    [Header("Boundary Check")]
+    public float horizontalBoundary = 2.3f;
+
+    private float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Move();
+        _CheckBounds();
+    }
+
+    private void _Move()
+    {
+        transform.position += new Vector3(direction * horizontalSpeed * Time.deltaTime, 0, 0);
+    }
+
+    private void _CheckBounds()
+    {
+        //check right wall
+        if (transform.position.x >= horizontalBoundary)
+        {
+            transform.position = new Vector3(horizontalBoundary, transform.position.y, 0.0f);
+            direction *= -1;
+        }
+
+        // check left wall
+        if (transform.position.x <= -horizontalBoundary)
+        {
+            transform.position = new Vector3(-horizontalBoundary, transform.position.y, 0.0f);
+            direction *= -1;
+        }
+    }
+}
